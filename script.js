de3d6f8a14be62dc0c9cf180d13c159ccc25eb94
theMungai <mungaithesimon@gmail.com>
2024-11-20 15:49:20 +0300

    Add To Cart

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 //  Looping through the "products" array to get products from the array instead of typing them manually in the HTML.
-const cart = []
 let productsHTML =  ""
 
 products.forEach((product) => {
@@ -23,49 +22,112 @@ products.forEach((product) => {
         </div>
     `;
 
-})
+    document.querySelector(".js-product-grid").innerHTML = productsHTML
 
-document.querySelector(".js-product-grid").innerHTML = productsHTML
 
-let cartQuantity = 0
-const addToCartButton = document.querySelectorAll(".add-to-cart-button");
 
-addToCartButton.forEach((button) => {
-    button.addEventListener("click", () => {
+    let cartQuantity = 0
+    const addToCartButton = document.querySelectorAll(".add-to-cart-button");
 
-        button.classList.add("js-clicked-button");
-        button.innerHTML = `
-            <img src="images/icon-decrement-quantity.svg" alt="" class = "minus-button svg-path">
+    addToCartButton.forEach((button) => {
+        button.addEventListener("click", () => {
+
+            button.classList.add("js-clicked-button");
+            button.innerHTML = `
+                <img src="images/icon-decrement-quantity.svg" alt="" class = "minus-button svg-path">
+                
+                <p> ${cartQuantity} </p>
+
+                <img src="images/icon-increment-quantity.svg" alt="" class = "plus-button svg-path">
+            `;
+
+            // Accessing and modifying plus icon
+            const plusIcon = button.querySelectorAll(".plus-button");
+            const minusIcon = button.querySelectorAll(".minus-button");
+            const emptyCart = document.querySelector(".cart-empty");
+            const cartWithItems = document.querySelector(".cart-with-items");
+
+            plusIcon.forEach((plusButton) => {
+                const cartWithItems = document.querySelector(".cart-with-items");
+                plusButton.addEventListener("click", () => {   
+                    emptyCart.style.display = "none";
+                    cartWithItems.style.display = "block";          
+                    cartQuantity = cartQuantity + 1;
+                    renderCart()
+                    
+                });
+                
+            });
+
+            // Accessing and modifying minus icon
             
-            <p> ${cartQuantity} </p>
-
-            <img src="images/icon-increment-quantity.svg" alt="" class = "plus-button svg-path">
-        `;
-
-        // Accessing and modifying plus icon
-        const plusIcon = button.querySelectorAll(".plus-button");
-
-        plusIcon.forEach((plusButton) => {
-            plusButton.addEventListener("click", () => {              
-                cartQuantity ++    
+            minusIcon.forEach((minusButton) => {
+                minusButton.addEventListener("click", () => {   
+                    if(cartQuantity > 0 ){
+                        cartQuantity = cartQuantity - 1
+                    }
+                });
             });
-        });
 
-        // Accessing and modifying minus icon
-        const minusIcon = button.querySelectorAll(".minus-button");
+            //  Generating Cart with JavaScript
+
+            const cart = []
+            let cartHTML = ""
+            
+            cart.forEach((cartItem) => {
+                cartHTML = cartHTML + `
+                    <div class="cart-items-container">
+                                        <div class="added-items">
+                                            <div class="full-product-in-cart">
+                                                <p style="color: hsl(14, 65%, 9%); font-weight: 500;"></p>
+                                            </div>
+
+                                            <div class="cart-price-product">
+                                                <span style="color: hsl(14, 86%, 42%); font-weight: 450; padding-right: 10px;">${cartQuantity}x</span>
+                                                <span style="color: hsl(7, 20%, 60%); padding: 0px 10px;">@ $${product.price.toFixed(2)}</span>
+                                                <span style="color: hsl(12, 20%, 44%);font-weight: 480">$${(product.price.toFixed(2)) * cartQuantity}</span>
+                                            </div>
+
+
+                                        </div>
+                                        <div class="remove-item">
+                                            <img src="/images/icon-remove-item.svg" alt="" class="remove-item-button">
+                                        </div>
+
+                                    </div>
+
+                                    <div class="total">
+                                        <p>Order Total</p>
+                                        <h1 class="full-cart-total">$46.50</h1>
+                                    </div>
+                                    <div class="carbon-neutral">
+                                <img src="images/icon-carbon-neutral.svg" alt="">
+                                <p>This is a <span>carbon-neutral</span> delivery</p>
+                            </div>
+
+                            <button class="order-button js-confirm-order">Confirm Order</button>
+                    </div>
         
-        minusIcon.forEach((minusButton) => {
-            minusButton.addEventListener("click", () => {   
-                if(cartQuantity > 0 ){
-                    cartQuantity --
-                }
+                `;
+                    cart.push(cartItem)
+                    console.log(cart)
             });
-        });
 
-        document.querySelector(".cart-quantity-display").innerHTML = cartQuantity   
-        
+            function renderCart() {
+                const addedItems = document.querySelector(".added-items");
+                cart.push(addedItems)
+            }
+            
+            cartWithItems.innerHTML = cartHTML
+
+            document.querySelector(".cart-quantity-display").innerHTML = cartQuantity   
+            
+        });
     });
+
 });
+
+
 
 
 
